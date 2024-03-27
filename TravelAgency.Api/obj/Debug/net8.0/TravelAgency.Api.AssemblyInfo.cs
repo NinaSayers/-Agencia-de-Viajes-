68f7c261d41f034bbefd62ae7f26e10984ad2114
Daniman30 <regulardani1@gmail.com>
2024-03-27 02:25:18 -0400

    fix bugs

--- TravelAgency.Api/obj/Debug/net8.0/TravelAgency.Api.AssemblyInfo.cs
+++ TravelAgency.Api/obj/Debug/net8.0/TravelAgency.Api.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelAgency.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e730b62581f467c5090e80df91bc5a54b17473")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ec8bf87830e3bc57d57c448327b9465615df04d")]
->>>>>>> 5356b3bda2e89b21c73be1e9a064870ce42cd8e4
 [assembly: System.Reflection.AssemblyProductAttribute("TravelAgency.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelAgency.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
